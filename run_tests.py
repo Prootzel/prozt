ee76dc8f7fa47396f083084edf27ccb6e757eaa0
Prootzel <lirkel123@gmail.com>
2024-11-21 14:25:56 +0100

    Added tests and coded functions

---
+++ run_tests.py
@@ -0,0 +1,1 @@
+from tests import print_tests
---
+++ tests/print_tests.py
@@ -0,0 +1,7 @@
+from prozt.prozt import prozt as print
+from prozt.prozt import prozt_rainbow
+from colorama import Fore, Back, Style
+
+
+print("Hello", "Worldwawawa!", fg_color = Fore.GREEN, bg_color=Back.WHITE)
+print("Ams proot :3")
